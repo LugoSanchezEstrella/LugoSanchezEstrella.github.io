52ae62cd842696d9907e3b8bc5c95b00d98631d6
LugoSanchezEstrella <159413856+LugoSanchezEstrella@users.noreply.github.com>
2024-08-30 18:43:34 -0700

    Práctica #1 30/08/24

---
+++ BaseDedatosWeb/parcial1/holamundo.php
@@ -0,0 +1,19 @@
+<?php
+
+$nombre = "Estrella";
+$numero = 4;
+$numero2= "4";
+
+$total = $numero + $numero2;
+
+echo "Total = " . $total;
+
+echo "<h1> HOLA MUNDO </h1>";
+
+for($i=0; $i<10; $i++){
+    echo $i . "<br>";
+}
+
+echo "<h1 style='color: purple;'> ESTRELLA LUGO SÁNCHEZ </h1>"
+
+?>
