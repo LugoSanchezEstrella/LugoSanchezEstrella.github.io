8e3a3f25cb6c28cacedc91664844b6a6710c45f5
LugoSanchezEstrella <159413856+LugoSanchezEstrella@users.noreply.github.com>
2024-10-30 18:06:48 -0700

    Add files via upload

--- BaseDedatosWeb/parcial2/practica9.js
+++ BaseDedatosWeb/parcial2/practica9.js
@@ -1,67 +1,67 @@
-$(document).ready(function(){
+$(document).ready(function (){
+    //Function click boton dar carta 
     let cartas = new Array(54).fill(false);
-    $("#dar_carta").click(function(){
-        var b = true;
-        var cont = 0;
-        while(b){
-            var num = Math.floor(Math.random()*54)+1;
-            console.log("Numero de carta="+num);
-            if(cartas[num-1]== true){
-                continue;
-            }else{
-                $("#carta_activa").html("<img src='imagenes/"+num+".jpg'>");
-                cartas[num-1] = true;
-                b=false;
-                cont++;
-            }
-            if(cont==54){
-                console.log("Ya se dieron todas las cartas");
-                b=false;
-            }
-        }
-    });
-     
-    cargarCartas();
-
-    function cargarCartas(){
-        var carta1 = 0;
-        var carta2 = 0;
-        let asignadas = new Array(54).fill(false);
-
-
-
-        while(carta1 < 16){
-            var num= Math.floor(Math.random()*54) + 1;
-            if(asignadas[num-1] == false){
-                $('#carta1').append("<div class='col-sm-3'><img class='img-fluid' src='imagenes/"+num+".jpg'></div>")
-               asignadas[num-1] = true;
-                carta1++;
-
-            }else{
-                continue;
-            }
-
-        }
+    var cont = 0;
+   $("#dar_carta").click (function(){
+    var b = true;
     
- 
+  
+  while(b){
+    var num= Math.floor(Math.random()*54) +1;
+    console.log("Numero de carta es"+num);
+    if(cartas[num-1] == true){
+      continue;
+    }else{
+        $("#carta_activa").html("<img src='imagenes/"+num+".jpg'>");
+        cartas[num-1] = true;
+        b= false;
+        cont++;
 
-
-    asignadas = new Array(54).fill(false);
-
-
-
-        while(carta2 < 16){
-            var num= Math.floor(Math.random()*54) + 1;
-            if(asignadas[num-1] == false){
-                $('#carta2').append("<div class='col-sm-3'><img class='img-fluid' src='imagenes/"+num+".jpg'></div>")
-               asignadas[num-1] = true;
-                carta2++;
-
-            }else{
-                continue;
-            }
-
-        }
     }
- 
- });
+    if(cont == 54){
+        console.log("ya se dieron todaslas cartas");
+        b= false;
+    }
+  }
+   });
+
+ cargaCartas();
+
+   function cargaCartas(){
+    var carta1 =0;
+    var carta2 =0;
+   let asignadas = new Array(54).fill(false);
+
+    while(carta1 < 16){
+    var num = Math.floor(Math.random()*54)+1;
+    if(asignadas[num-1] == false){
+      $('#carta1').append("<div class='col-sm-3'><img class='img-fluid'  src='imagenes/"+num+".jpg'></div>")
+      asignadas[num-1]  = true;
+      carta1++;
+    }else{ 
+      continue;
+    
+         }
+
+     }
+
+   
+
+   asignadas = new Array(54).fill(false);
+
+    while(carta2 < 16){
+    var num = Math.floor(Math.random()*54)+1;
+    if(asignadas[num-1] == false){
+      $('#carta2').append("<div class='col-sm-3'><img class='img-fluid'  src='imagenes/"+num+".jpg'></div>")
+      asignadas[num-1]  = true;
+      carta2++;
+    } else { 
+      continue;
+    
+         }
+
+     }
+
+   }
+
+});
